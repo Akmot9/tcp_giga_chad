b435b5653f260838052cee2192379d618a68294e
Akmot9 <avicocyprien@yahoo.com>
2024-02-20 00:03:54 +0100

    docker compose

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read, Write};
+use std::net::TcpStream;
+use std::str;
+
+fn main() -> io::Result<()> {
+    let mut stream = TcpStream::connect("server:8080")?;
+
+    stream.write_all(b"Hello from client")?;
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer)?;
+    println!("Received: {}", str::from_utf8(&buffer).unwrap().trim_end());
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::str;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    while match stream.read(&mut buffer) {
+        Ok(size) => {
+            if size == 0 {
+                // Connexion fermée
+                return;
+            }
+            println!("Received: {}", str::from_utf8(&buffer[..size]).unwrap());
+            true
+        }
+        Err(e) => {
+            eprintln!("An error occurred, terminating connection with {}: {}", stream.peer_addr().unwrap(), e);
+            stream.shutdown(std::net::Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8080")?;
+    println!("Server listening on port 8080");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move || {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                eprintln!("Connection failed: {}", e);
+            }
+        }
+    }
+    Ok(())
 }
